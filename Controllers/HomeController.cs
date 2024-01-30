46e74703e1d004e822f28d01bac0b02e5bd33b4a
DESKTOP-JI1JKUA\ibrahim <donmez2k@gmail.com>
2024-01-30 20:08:15 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using CasheOrnekProje.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Diagnostics;
+
+namespace CasheOrnekProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMemoryCache _cache;
+
+        public HomeController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            DateTime cacheEntry;
+            string cacheKey = "zaman";
+            if (!_cache.TryGetValue(cacheKey, out cacheEntry))
+            {
+                cacheEntry = DateTime.Now;
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+    .SetSlidingExpiration(TimeSpan.FromSeconds(30))
+    .SetAbsoluteExpiration(TimeSpan.FromMinutes(1))
+    .SetPriority(CacheItemPriority.Normal);
+                _cache.Set(cacheKey, cacheEntry, cacheEntryOptions);
+
+            }
+
+            return View(cacheEntry);
+        }
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CasheOrnekProje.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
